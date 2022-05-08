2d0b743816dc231108dac10ec635bc677e831d70
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-08 16:05:42 -0300

    Add error messages in api

--- pages/api/link.js
+++ pages/api/link.js
@@ -1,20 +1,24 @@
-import { clearPhoneMask } from "../../controllers/phoneController";
-import targets from "../../static/targets";
+import { clearPhoneMask } from '../../controllers/phoneController';
+import targets from '../../static/targets';
 
 export default function handler({ query }, res) {
   try {
     const { phone, message, target } = query;
 
-    if (phone && message && target) {
-      const subdomain = targets.filter(({ title }) => title === target)[0].subdomain;
-      const phoneNumber = clearPhoneMask(phone);
-      const link = `https://${subdomain}.whatsapp.com/send?phone=55${phoneNumber}&text=${message}`;
-
-      res.status(200).json({ error: false, link });
-    } else {
-      throw new Error();
+    if (!phone || !message || !target) {
+      throw new Error('Os parâmetros `phone`, `message` e `target` são obrigatórios');
     }
-  } catch (error) {
-    res.status(400).json({ error: true });
+
+    const foundTarget = targets.filter(({ short }) => short === target)[0];
+
+    if (!foundTarget) throw new Error('O parâmetro `target` é inválido');
+
+    const subdomain = foundTarget.subdomain;
+    const phoneNumber = clearPhoneMask(phone);
+    const link = `https://${subdomain}.whatsapp.com/send?phone=55${phoneNumber}&text=${message}`;
+
+    res.status(200).json({ link });
+  } catch ({ message }) {
+    res.status(400).json({ error: message });
   }
 }
